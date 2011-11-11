920830ca8b676925836b40beef3d7155161d9807
sa <sanami77@gmail.com>
2011-11-11 18:31:46 +0200

    Parse form inputs

---
+++ form.rb
@@ -0,0 +1,90 @@
+require 'pp'
+require 'nokogiri'
+require './tools.rb'
+
+def get_name(dat)
+	all = []
+	rx_href = /name=["'](.+?)["']/
+	rx_param_name = /\[/
+
+	dat.scan(rx_href) do |matches|
+		#pp matches
+		name = matches[0]
+
+		if name =~ rx_param_name
+			all << name
+		end
+	end
+
+	all
+end
+
+def get_attribute(all, el, type)
+	# "company[postal_address]"
+	#rx_name = /(.+?)(\[(.+?)\])+/
+	#rx_name.match(el['name']) do |m|
+
+	rx_name = /(.+?)\[(.+?)\]/
+
+	if el['name'] =~ rx_name
+		model_name = $1
+		attr = $2
+
+		all[model_name] ||= {}
+		all[model_name][attr] = type
+	else
+		puts "skip: #{el.name} #{el['name']}"
+	end
+end
+
+def parse_html(dat)
+	all = {} # model_name => {attr => type, ...}
+	doc = Nokogiri::HTML(dat)
+
+	(doc/'input, select, textarea').each do |el|
+		#pp el
+		type = case el.name
+			when 'input'
+				el['type']
+			else
+				el.name
+		end
+		get_attribute(all, el, type)
+	end
+
+	all
+end
+
+def generate_scheme(model, attrs)
+	puts "rails g scaffold #{model.capitalize}"
+	#name:string year:integer
+	attrs.each do |name, ctrl|
+		type = 'string'
+		if ctrl == 'checkbox'
+			type = 'boolean'
+		elsif ctrl == 'textarea'
+			type = 'text'
+		elsif ctrl == 'select' || ctrl == 'number' || name.end_with?('_id')
+			type = 'integer'
+		end
+		puts "\t#{name}:#{type}"
+
+	end
+end
+
+def parse_file(file_path)
+	dat = File.read file_path
+	fix_encoding! dat
+
+	#pp get_name(dat)
+	all = parse_html(dat)
+
+	all.each do |model, attrs|
+		generate_scheme(model, attrs)
+	end
+end
+
+#file_path = '/home/sa/projects/copycat/tmp/bar.drincash.com/office/company/edit'
+file_path = '/home/sa/projects/copycat/tmp/bar.drincash.com/office/company/card'
+
+parse_file(file_path)
--- tools.rb
+++ tools.rb
@@ -6,7 +6,5 @@ def fix_encoding!(str)
 	#str.force_encoding("UTF-8")
 	#pp str.encoding, str.valid_encoding?
 	str.encode!("UTF-16BE", :invalid=>:replace, :replace=>"?")
-	#pp str.encoding, str.valid_encoding?
 	str.encode!("UTF-8")
-	#pp str.encoding, str.valid_encoding?
 end
